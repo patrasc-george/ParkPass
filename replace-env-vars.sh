f26d026bb3c86b8de377d9578129905abe6a14f8
patrasc-george <georgedanielp14@gmail.com>
2025-02-13 11:17:24 +0200

    Bug fixed.

--- replace-env-vars.sh
+++ replace-env-vars.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sed -i "s|{{API_URL}}|$API_URL|g" /usr/share/nginx/html/index.html
-sed -i "s|{{POSTGRES_PASSWORD}}|$POSTGRES_PASSWORD|g" /usr/share/nginx/html/index.html
+sed -i "s|{{API_URL}}|${API_URL}|g" /usr/share/nginx/html/index.html
+sed -i "s|{{POSTGRES_PASSWORD}}|${POSTGRES_PASSWORD}|g" /usr/share/nginx/html/index.html
